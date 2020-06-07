032b05533d0181e8087c6b57f5bb95b7477d85a1
Bill Hunt <bill@krues8dr.com>
2020-06-07 18:52:31 -0400

    Remove unused setting.

--- settings.example.py
+++ settings.example.py
@@ -1,8 +1,5 @@
 # Your Github token.  Needs the "repo" permission.
 GITHUB_TOKEN = ''
 
-# The destination user or organization on Github.
-GITHUB_ORG = ''
-
 # The new branch name to use
 BRANCH = 'release'
