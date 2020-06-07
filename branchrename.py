057863165a3eb60eab06ff0fce8a83be97d12bce
Bill Hunt <bill@krues8dr.com>
2020-06-07 14:45:14 -0400

    Initial commit.

---
+++ branchrename.py
@@ -0,0 +1,45 @@
+from github import Github, GithubException
+
+import settings
+
+
+g = Github(settings.GITHUB_TOKEN)
+
+
+oldrepo = 'master'
+newrepo = settings.BRANCH
+
+ulogin = g.get_user().login
+
+# We only want repos that we own and are not forks.
+for repo in g.get_user().get_repos():
+  if ulogin == repo.owner.login and repo.fork == False:
+
+    try:
+      branch = repo.get_branch(branch=oldrepo)
+
+    except GithubException:
+      continue # We expect this if there's no master branch.
+
+    print(repo.name)
+
+    # If we already have a branch named after the new branch, skip this repo.
+    try:
+      branch = repo.get_branch(branch=newrepo)
+      if branch:
+        print('Branch %s already exists, skipping' % newrepo)
+        continue
+
+    except GithubException:
+      pass # We expect this.
+
+    src = repo.get_git_ref('heads/%s' % oldrepo)
+    print(src.object.sha)
+
+    repo.create_git_ref('refs/heads/%s' % newrepo, sha=src.object.sha)
+
+    src.delete()
+
+    print('Done')
+    print()
+
---
+++ settings.example.py
@@ -0,0 +1,8 @@
+# Your Github token.  Needs the "repo" permission.
+GITHUB_TOKEN = ''
+
+# The destination user or organization on Github.
+GITHUB_ORG = ''
+
+# The new branch name to use
+BRANCH = 'release'
